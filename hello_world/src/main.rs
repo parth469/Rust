c58d16e0022c33e76eb5be447cbbea3e335d1b0c
parth469 <parthmain2001@gmail.com>
2023-08-21 20:07:14 +0530

    - Learn Hello World

---
+++ hello_world/src/main.rs
@@ -0,0 +1,10 @@
+// * main is first function which will run first
+fn main() {
+    // *  println! calls -> Rust macro.
+    // ?  "!" use to identified that we call macro
+    // *  ; Use for expression is over
+    println!("Hello, world!");
+}
+
+// ! Result :-
+// * When we run this code we are able to see  Hello, world! print on you screen
